88824934df00457b005b31136c9272fe2c5c1b38
Zhen Yang <webyangfullstack@gmail.com>
2022-11-13 19:16:56 +1100

    create loading section

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,7 +8,24 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 
 const upload = () => {
-  return <div>upload</div>;
+	const [isLoading, setIsLoading] = useState(false);
+  return (
+    <div className="flex w-full h-full">
+      <div className="bg-white rounded-lg">
+        <div>
+          <div>
+            <p className="text-2xl font-bold">Upload Video</p>
+            <p className="text-md text-gray-400 mt-1">
+              Post video to your account
+            </p>
+          </div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+            {isLoading ? <p>Uploading...</p> : <div></div>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default upload;
