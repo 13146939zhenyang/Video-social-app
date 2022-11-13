eb9840290a0b97aef427b28a69f2178c4caf4e43
Zhen Yang <webyangfullstack@gmail.com>
2022-11-13 19:08:34 +1100

    create upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
+
+import useAuthStore from "../store/authStore";
+import { client } from "../utils/client";
 
 const upload = () => {
   return <div>upload</div>;
