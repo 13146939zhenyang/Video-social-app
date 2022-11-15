3a344ec5ac332a212707f1a80a00617af45e8a54
Zhen Yang <webyangfullstack@gmail.com>
2022-11-15 22:49:03 +1100

    add margin of the screen

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillFilePlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from "axios";
 
 const Detail = () => {
-  return (
-	<div>Detail</div>
-  )
-}
+  return <div>Detail</div>;
+};
 
-export default Detail
+export default Detail; 
