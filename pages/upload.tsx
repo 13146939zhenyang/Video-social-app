fc08dc484053e72cff15b196c7259d389488a3ee
Zhen Yang <webyangfullstack@gmail.com>
2022-11-15 19:22:39 +1100

    uploading section css

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,6 +9,7 @@ import { client } from "../utils/client";
 
 const upload = () => {
 	const [isLoading, setIsLoading] = useState(false);
+	const videoAsset = false;
   return (
     <div className="flex w-full h-full">
       <div className="bg-white rounded-lg">
@@ -20,7 +21,35 @@ const upload = () => {
             </p>
           </div>
           <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
-            {isLoading ? <p>Uploading...</p> : <div></div>}
+            {isLoading ? (
+              <p>Uploading...</p>
+            ) : (
+              <div>
+                {videoAsset ? (
+                  <div></div>
+                ) : (
+                  <label className="curor-pointer">
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <div className="flex flex-col items-center justify-center">
+                        <p>
+                          <FaCloudUploadAlt className="text-gray-300 text-6xl" />
+                        </p>
+                        <p className="text-gray-300 text-lg mt-2">
+                          Upload Video
+                        </p>
+                      </div>
+                      <p className="text-gray-400 text-center mt-10 text-sm leading-10">
+                        MP4 or WebM or ogg
+                        <br />
+                        720x1280 or 1080x1920
+                        <br />
+                        10MB or less
+                      </p>
+                    </div>
+                  </label>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
