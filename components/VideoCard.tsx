e1463889f432ac7dada4dd26dcde8508d858d93e
Zhen Yang <webyangfullstack@gmail.com>
2022-11-15 21:28:54 +1100

    create route to go to the detail page of each video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -69,7 +69,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             setIsHover(false);
           }}
         >
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               loop
               ref={videoRef}
